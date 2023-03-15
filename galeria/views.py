890eeb4d63d3d42722e252d60d8c1f6830d1df62
Daniel Ferraz <danielferrazmm@hotmail.com>
2023-03-14 21:26:45 -0300

    primeira parte do curso de django da Alura

---
+++ galeria/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'galeria/index.html')
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
